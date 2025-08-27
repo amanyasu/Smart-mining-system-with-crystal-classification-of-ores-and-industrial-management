afa58e781fedc254e50af0704cc7b55667af8bc1
Yashu Youwaraj <amanyasu@gmail.com>
2025-08-27 17:45:02 +0530

    Initial commit

---
+++ utils/disease.py
@@ -0,0 +1,150 @@
+disease_dic = {
+    'Carnalite___Cedar_apple_rust': """ <b>Crop</b>: Carnalite <br/>Properties: Cedar Carnalite Rust<br/>
+        <br/> Cause of Properties:
+
+        <br/><br/>Cedar apple rust (Gymnosporangium juniperi-virginianae) is a fungal disease that depends on two species to spread and develop. It spends a portion of its two-year life cycle on Eastern red cedar (Juniperus virginiana). The pathogen’s spores develop in late fall on the juniper as a reddish brown gall on young branches of the trees.
+
+        <br/><br/> How to prevent/cure the ores <br/>
+        <br/>1. Since the juniper galls are the source of the spores that infect the apple trees, cutting them is a sound strategy if there aren’t too many of them.
+
+        <br/>2. While the spores can travel for miles, most of the ones that could infect your tree are within a few hundred feet.
+        
+        <br/>3. The best way to do this is to prune the branches about 4-6 inches below the galls.""",
+
+
+
+    'Saltpetre___Potassium': """ <b>Crop</b>: Saltpetre <br/>Disease: Saltpetre<br/>
+
+        <br/><br/> Chile saltpetre is the common name for sodium nitrate, it is called chile saltpetre because it is a deliquescent.
+        <br/>crystalline sodium salt that is found chiefly in northern Chile.""",
+
+
+    'Rocksalt_(including_sour)': """ <b>Crop</b>: Rocksalt <br/>Disease: Rock Salt<br/>
+        <br/> Cause of Ores:
+
+        <br/><br/>Podosphaera clandestina, a fungus that most commonly infects young, expanding leaves but can also be found on buds, fruit and fruit stems. It overwinters as small, round, black bodies (chasmothecia) on dead leaves, on the orchard floor, or in tree crotches. Colonies produce more (asexual) spores generally around shuck fall and continue the disease cycle.
+
+
+        <br/><br/> How to prevent/cure the ores <br/>
+        <br/>1. Remove and destroy sucker shoots.
+
+        <br/>2. Keep irrigation water off developing fruit and leaves by using irrigation that does not wet the leaves. Also, keep irrigation sets as short as possible.
+        
+        <br/>3. Follow cultural practices that promote good air circulation, such as pruning, and moderate shoot growth through judicious nitrogen management.""",
+
+
+    'Dolomite___Cercospora_leaf_spot Gray_leaf_spot': """ <b>Crop</b>: Dolomite <br/>Disease: Automic Number:42<br/>
+        <br/> Cause of disease:
+
+        <br/><br/>Dolomite is a type of limestone. It is rich in magnesium carbonate and calcium carbonate. .
+
+
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. It also contains several other minerals. Dolomite is made of 60% calcium carbonate and 40% magnesium carbonate..
+
+        <br/>2. Dolomite is used as a source of magnesium metal and of magnesia (MgO), which is a constituent of refractory bricks.
+        
+        <br/>3. High risk factors for grey leaf spot in corn: <br/>
+                    Density: 2.84 g/cm³ (Average)
+                    Crystal system: Trigonal
+                    Cleavage: 3 directions of cleavage not at right angles
+                    Color: White, grey to pink, reddish-white, brownish-white; colourless in transmitted light
+                    Luster: Vitreous to pearly
+                    Solubility: Poorly soluble in dilute HCl
+                    Specific gravity: 2.84–2.86.""",
+
+    'Cinnabar___Haunglongbing_(Citrus_greening)': """ <b>Crop</b>: Cinnabar <br/>Disease: Automic Number:55<br/>
+        <br/> Cause of disease:
+
+        <br/><br/>  Cinnabar, or cinnabarite, is the bright scarlet to brick-red form of mercury(II) sulfide. It is the most common source ore for refining elemental mercury and is the historic source for the brilliant red or scarlet pigment termed vermilion and associated red mercury pigments.
+
+
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. Metacinnabar, the isometric (cubic) form of cinnabar, transforms to cinnabar upon heating to 400°–550° C (750°–1,020° F).
+
+        <br/>2. Formula:HgSColour:Tint or shade of red; cochineal red, brownish red, silvery dark red; silvery-greyLustre:MetallicHardness:2 - 2½Specific Gravity:8.176Crystal System:TrigonalName:The origin of the name is still unclear, but beyond doubt oriental (Lüschen, 1979, p. 348)..
+        
+        <br/>3. The most striking property of cinnabar is its red color. Its bright color makes it easy to spot in the field and is a fascination for those who discover it. It has a Mohs hardness of 2 to 2.5 and is very easily ground into a very fine powder. It has a specific gravity of 8.1, which is extremely high for a nonmetallic mineral""",
+
+
+
+    'Anhydrite-2-1___Bacterial_spot': """ <b>Crop</b>: Anhydrite-2-1 <br/>Disease: Automic Number:21<br/>
+        <br/> Cause of disease:
+
+        <br/><br/> 1. Anhydrite is one of the major minerals in evaporite deposits; it also is present in dolomites and limestones, and as a gangue mineral in ore veins. It is used in plasters and cement as a drying agent. Anhydrite crystals possess orthorhombic symmetry. For physical properties, see sulfate mineral (table).
+
+        <br/> 2. Anhydrite is a crystalline mineral, chemically calcium sulphate – CaSO4. Like other evaporite minerals, it is formed by the deposition of brine in sedimentation basins.
+
+
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. Anhydrite, or anhydrous calcium sulfate, is a mineral with the chemical formula CaSO4!
+
+        <br/>2. It is a member of the orthorhombic crystal system, exhibiting three directions of perfect cleavage coincident with the three planes of symmetry.
+        
+        <br/>3. Anhydrite is a mineral anhydrous calcium sulfate, CaSO4.
+        <br/>4. Formula
+                (repeating unit)	CaSO4
+                IMA symbol	Anh[1]
+                Strunz classification	7.AD.30
+                Dana classification	28.3.2.1
+                Crystal system	Orthorhombic.""",
+
+    'Anglesite___healthy': """ <b>Crop</b>: Anglesite <br/>Disease: Anglesite<br/>
+
+        <br/><br/> 1.Anglesite is a lead sulfate mineral with the chemical formula PbSO₄. It occurs as an oxidation product of primary lead sulfide ore, galena. 
+        
+        <br/>2.Cleavage on the basal plane of a prism [001] is good, while the faces of the prism (110} are distinct.
+        
+        <br/>3.Anglesite occurs as prismatic orthorhombic crystals and earthy masses, and is isomorphous with barite and celestine. !!!""",
+
+    'Galena___healthy': """ <b>Crop</b>: Galena <br/>Properties: Atomic Number 56<br/>
+
+        <br/><br/> Galena is the main ore of lead, used since ancient times, since lead can be smelted from galena in an ordinary wood fire. 
+        <br/>Galena typically is found in hydrothermal veins in association with sphalerite, marcasite, chalcopyrite, cerussite, anglesite, dolomite, calcite, quartz, barite, and fluorite. !!!""",
+
+
+
+    'sylvanite___Leaf_scorch': """ <b>Crop</b>: sylvanite <br/>Disease: Atomic number : 87<br/>
+        <br/> Cause of disease:
+
+        <br/><br/> 1. Sylvanite, a gold and silver telluride mineral [(Au,Ag)Te2] in which the ratio of gold to silver atoms is commonly close to 1:1. It is a member of the krennerite group of sulfides and is found associated with them in ore veins formed at low temperatures in Hungary, Australia, Canada, and the western United States. It forms monoclinic crystals.
+
+        <br/> 2. a silver-white mineral consisting of a telluride of gold and silver in the form of elongated striated crystals: a source of gold in Australia and North America. Formula: (Au,Ag)Te 2.
+
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. a mineral (Au,Ag)Te2 consisting of a gold silver telluride, having a steel gray, .
+
+        <br/>2. silver white, or brass yellow color, and often occurring in implanted crystals resembling written characters (hardness 1.5–2 specific gravity 7.9–8.3).
+        
+        <br/>3. This sample of sylvanite is displayed in the Smithsonian Museum of Natural History. Sylvanite is a mineral composed of gold, silver and tellurium. It has the composition AgAuTe4. This sample is about 20 cm wide and is from Sacaramb, Romania..""",
+
+
+    'Tungsten___Early_blight': """ <b>Crop</b>: Tungsten <br/>Properties: atomic number 74<br/>
+        <br/> Cause of Ores:
+
+        <br/><br/>  Tungsten has the highest melting point and lowest vapor pressure of all metals, and at temperatures over 1650°C has the highest tensile strength. It has excellent corrosion resistance and is attacked only slightly by most mineral acids..
+        <br/> 2. Alternaria Tungstenphila is more virulent on Tungsten than A. solani, so in regions where A. Tungstenphila is found, it is the primary cause of early blight on Tungsten. However, if A.Tungstenphila is absent, A.solani will cause early blight on Tungsten.
+        <br/><br/> How to prevent/cure the issues of the ores <br/>
+        <br/>1. Use pathogen-free seed, or collect seed only from disease-free plants..
+
+        <br/>2. Rotate out of Tungstenes and related Products for at least two years.
+        
+        <br/>3. Control susceptible weeds such as black nightshade and hairy nightshade, and volunteer Tungsten plants throughout the rotation.
+        <br/>4. Fertilize properly to maintain vigorous plant growth. Particularly, do not over-fertilize with potassium and maintain adequate levels of both nitrogen and phosphorus.
+        <br/>5. Avoid working in plants when they are wet from rain, irrigation, or dew.
+        <br/>6. Use drip irrigation instead of overhead irrigation to keep foliage dry.""",
+
+
+
+    'Chlorargyrite___Late_blight': """ <b>Crop</b>: Chlorargyrite <br/>Disease: Automic Number 24 <br/>
+
+        Chlorargyrite is the mineral form of silver chloride (AgCl).[5] Chlorargyrite occurs as a secondary mineral phase in the oxidation of silver mineral deposits. It crystallizes in the isometric–hexoctahedral crystal class. Typically massive to columnar in occurrence it also has been found as colorless to variably yellow cubic crystals. 
+                      
+        <br/> Cause of disease:
+
+        <br/><br/> 1. The color changes to brown or purple on exposure to light. It is quite soft with a Mohs hardness of 1 to 2 and dense with a specific gravity of 5.55..
+
+        <br/> 2.Chlorargyrite occurs as a secondary mineral phase in the oxidation of silver mineral deposits. or the other.
+        <br/> 3.  Locality: Chañarcillo, Copiapó Province, Atacama Region, Chile (Locality at mindat.org) Size: 5.0 x 4.7 x 1.0 cm. An incredible, old-time specimen from Chanarcillo of a solid sheet of waxy lustre, yellow-green, bromian chlorargyrite microcrystals on a thin, platy matrix crust. .""",
+
+}
